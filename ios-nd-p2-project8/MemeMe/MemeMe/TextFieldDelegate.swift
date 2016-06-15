eca1047ed8ed8e8176fbada2bb1c7cd274c765c3
Juan Alvarez <juanalvarezmartinez@gmail.com>
2016-06-15 13:45:08 -0400

    Add the Meme me project

---
+++ ios-nd-p2-project8/MemeMe/MemeMe/TextFieldDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  TextFieldDelegate.swift
+//  MemeMe
+//
+//  Created by Juan Alvarez on 14/6/16.
+//  Copyright © 2016 Juan Alvarez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TextFieldDelegate: NSObject, UITextFieldDelegate {
+    
+    //var previousText: String! = ""
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        
+        // Construct the text that will be in the field if this change is accepted
+        var newText = textField.text! as NSString
+        newText = newText.stringByReplacingCharactersInRange(range, withString: string)
+        
+        return newText.length <= 50
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        
+        //previousText = textField.text!
+        if ((textField.text == "TOP") || textField.text == "BOTTOM" ) {
+            textField.text = ""
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+}
